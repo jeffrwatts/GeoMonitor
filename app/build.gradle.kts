5327abfeda7b0188319bdc9b2cbbe13fefc5d240
jeffrwatts <jeffrwatts@gmail.com>
2024-05-28 18:30:03 -0700

    update gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,25 @@
+import java.util.Properties
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    //id("com.google.devtools.ksp") version "1.9.10-1.0.13"
+    kotlin("kapt") // Needed for hilt currently.
+    id("com.google.dagger.hilt.android")
+    kotlin("plugin.allopen") version "1.9.10"
+}
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
 }
 
 android {
     namespace = "com.jeffrwatts.geomonitor"
     compileSdk = 34
+
+    val apiUrl = localProperties.getProperty("api_base_url", "https://default.api.url/")
 
     defaultConfig {
         applicationId = "com.jeffrwatts.geomonitor"
@@ -20,6 +34,11 @@ android {
         }
     }
 
+    buildFeatures {
+        // Enable custom BuildConfig fields
+        buildConfig = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,20 +46,25 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "API_BASE_URL", apiUrl)
+        }
+
+        debug {
+            buildConfigField("String", "API_BASE_URL", apiUrl)
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -50,20 +74,73 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
+
+    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    // Google Play Services for Location.
+    implementation ("com.google.android.gms:play-services-location:21.2.0")
+
+    // Dependency Injection
+    implementation ("com.google.dagger:hilt-android:2.49")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation ("androidx.hilt:hilt-work:1.2.0")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    kapt ("com.google.dagger:hilt-compiler:2.49")
+
+    // Permissions
+    implementation ("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    // Serialization
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // retrofit for networking
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    testImplementation ("androidx.room:room-testing:2.6.1")
+
+
+    // Unit tests
+    testImplementation("junit:junit:4.13.2")
+
+    // Android Instrumentation tests
+    androidTestImplementation ("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation ("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // Mockito
+    testImplementation ("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0") // Unit tests are broken if upgrade to 5.8.0.
+    androidTestImplementation ("org.mockito:mockito-android:5.8.0")
 }
